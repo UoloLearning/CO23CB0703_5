ba1ba8f82fa0cf99e0e37aa14d90b6d4288f1b61
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-24 19:07:47 +0530

    Update CO23CB0703_5_A1.py

--- CO23CB0703_5_A1.py
+++ CO23CB0703_5_A1.py
@@ -1,1 +1,17 @@
+def count_vowels_and_consonants(word):
+    """This function counts the number of vowels and consonants in a word."""
+    num_vowels = 0
+    num_consonants = 0
+    for letter in word:
+        if letter.lower() == 'a' or letter.lower() == 'e' or letter.lower() == 'i' or letter.lower() == 'o' or letter.lower() == 'u':
+            num_vowels += 1
+        else:
+            num_consonants += 1
+    return num_vowels, num_consonants
 
+# Call the count_vowels_and_consonants function with an argument of "hello"
+vowels, consonents = count_vowels_and_consonants("abcdefghijklmnopqrstuvwxyz")
+
+# Print the result
+print("Number of vowels:", vowels)
+print("Number of consonants:", consonents)
